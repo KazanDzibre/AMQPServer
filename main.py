a7bd2c9a10e4586e0196d4e3d99f0fbd187333f4
KazanDzibre <markoruzic997@gmail.com>
2021-03-03 17:00:03 +0100

    uspostavljena ipv6 konekcija

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import socket
+import sys
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0)
 
-sock.bind(('localhost',5671))
+server_address = ('localhost', 10000)
+sock.bind(server_address)
 
-sock.listen()
+sock.listen(1)
 
-c = sock.accept()
+while True:
+    # Wait for connection
+    connection, client_address = sock.accept()
+
+    try:
+
+        # Receive the data in small chunks and retransmit it
+        while True:
+            data = connection.recv(64)
+            print(data)
+            if data:
+                connection.sendall(data)
+            else:
+                break
+
+    finally:
+        # Clean up the connection
+        connection.close()
