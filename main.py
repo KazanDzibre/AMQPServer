bbc5b88261ec2f2b777aca38bcea6780120140f6
Jovanovski Sebastian <sebastiangenorth@gmail.com>
2021-03-11 00:22:20 +0100

    krenuo da radim start funkciju

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import socket
-import sys
+import struct
+from pika.frame import ProtocolHeader
+from pika.connection import Connection
+
+def decode(data):
+    # Look to see if it's a protocol header frame
+    try:
+        if data[0:4] == b'AMQP':
+            major, minor, revision = struct.unpack_from('BBB', data, 5)
+            return 8, ProtocolHeader(major, minor, revision)
+    except (IndexError, struct.error):
+        return 0, None
+    # Get the Frame Type, channel Number and Frame Size
+    try:
+        (frame_type, channel_number, frame_size) = struct.unpack('>BHL',data[0:7])
+    except struct.error:
+        return 0, None
+
 
 HOSTNAME = 'localhost'
 DEFAULT_PORT = 6000
@@ -13,9 +30,12 @@ sock.bind(server_address)
 print("server opened socket connection")
 sock.listen(1)
 
-connection, client_address = sock.accept()
+client_sock, client_address = sock.accept()
 
 print("Server connected by", client_address)
-data = connection.recv(MAX_BYTES, 0)
+data = client_sock.recv(MAX_BYTES, 0)
 print(data)
 
+byte_rec, PH = decode(data)
+
+Connection._send_connection_start()
