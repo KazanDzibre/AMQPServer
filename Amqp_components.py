d32474f3abd086d35dbdeaef64eb40cd920d725c
Jovanovski Sebastian <sebastiangenorth@gmail.com>
2021-03-20 18:35:48 +0100

    sad sam zapravo komitovo ovaj fajl xD

---
+++ Amqp_components.py
@@ -0,0 +1,28 @@
+from pika.exchange_type import ExchangeType
+
+
+class AmqpQueue():
+    queue = []
+
+    def __init__(self, name=''):
+        self.name = name
+        # self.auto_delete = auto_delete
+        # self.durable = durable
+        # self.exclusive = exclusive
+        # self.synchronous = synchronous
+
+    def append_to_queue(self, message):
+        self.queue.append(message)
+
+    def pop_from_queue(self):
+        return self.queue.pop()
+
+    def print_queue(self):
+        print(self.queue)
+
+
+class AmqpExchange():
+    exchange = ''
+    exchange_type = None
+
+
